ab5636ef67ba33f4040072e4dcbe80740126f9e9
Imran Artwel <imranartweljunior@gmail.com>
2017-06-22 21:45:32 +0800

    final commit

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ app.use('/api',api);
 
 //Index Route
 app.get('/', (req, res) => {
-  res.send('Invalid Endpoint');
-});
-
-app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 
 app.listen(port, function () {
